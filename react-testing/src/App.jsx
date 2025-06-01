3dab9a6f0fd43f5a29e6194aec6267d181ac1d4b
Jodi Jonatan <jdi102350@gmail.com>
2025-06-01 20:39:41 +0700

    change App jsx

--- react-testing/src/App.jsx
+++ react-testing/src/App.jsx
@@ -11,23 +11,20 @@ import Counter from "./form/Counter";
 
 function SayHello ({text = 'Anontmous'}) {
   return (
-    <h2 className="text-sky-700 font-bold text-2xl">Hello, {text}, <span className="text-black font-normal text-md">nice to meet you!</span></h2>
+    <h2 className="text-sky-700 font-bold text-2xl">Hello vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv, {text}, <span className="text-black font-normal text-md">nice to meet you!</span></h2>
   )
 }
 
 export default function App () {
-  const author = "Jodi Jonatan Karo karo"
   const [count, setCount] = useState(0)
 
   return (
     <div className="min-h-screen bg-slate-600 text-center py-16">
       <h2 className="font-bold text-xl">Hello, World!</h2>
       <p>Belajar React Dasar ✌️</p>
-      <h2>{author.toUpperCase()}</h2>
-      <button className="bg-sky-200 py-1 px-2 rounded-full cursor-pointer" onClick={() => setCount((count) => count + 1)}>
+      <button className="bg-sky-200 py-1 w-16 rounded-full cursor-pointer" onClick={() => setCount((count) => count + 1)}>
         Like {count}
       </button>
-      <SayHello text={author} />
       <TodoList />
       <Table />
       <AlertButton text="Click Me" message="Tombol telah diklik"/> <br/>
